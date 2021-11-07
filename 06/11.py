52d92a50d0ade5132f4304baf751288afaebea92
KacperKowalczyk0102 <92937759+KacperKowalczyk0102@users.noreply.github.com>
2021-11-07 11:19:05 +0100

    Add files via upload

---
+++ 06/11.py
@@ -0,0 +1,10 @@
+arr1 = ["water", "book", "sky"]
+arr2 = ["water", "book", "sky"]
+
+def compare(arr1,arr2):
+    if len(arr2) == len(arr1):
+        for index in range(len(arr1)):
+            if not arr1
+        return True
+    else:
+        return False
