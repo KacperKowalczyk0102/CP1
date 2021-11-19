74a94eb3718074eb2c418e034874997d1b215bac
KacperKowalczyk0102 <92937759+KacperKowalczyk0102@users.noreply.github.com>
2021-11-19 20:48:37 +0100

    Add files via upload

---
+++ 08/8.py
@@ -0,0 +1,11 @@
+phones = {
+    "producent": 'apple',
+    "model": 'se 2020',
+    "color": True,
+    "size": 12,
+    "memory": 64,
+    "ram memory": 3
+    }
+
+for key,value in phones.items():
+    print(f'{key} = {value}')
